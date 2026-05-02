f965632b3cae1d4de5a147bad568d1628ed222fa
Achintya Dwivedi <achintyadwivedi09@gmail.com>
2026-05-02 11:39:57 +0530

    Update scheduler.js

--- vehicle_maintenance_scheduler/scheduler.js
+++ vehicle_maintenance_scheduler/scheduler.js
@@ -1,24 +1,24 @@
-const scheduleVehicles = (vehicles, maxHours) => {
-    let n = vehicles.length;
+// const scheduleVehicles = (vehicles, maxHours) => {
+//     let n = vehicles.length;
 
-    let dp = Array(n + 1).fill().map(() => Array(maxHours + 1).fill(0));
+//     let dp = Array(n + 1).fill().map(() => Array(maxHours + 1).fill(0));
 
-    for (let i = 1; i <= n; i++) {
-        let { duration, impact } = vehicles[i - 1];
+//     for (let i = 1; i <= n; i++) {
+//         let { duration, impact } = vehicles[i - 1];
 
-        for (let w = 0; w <= maxHours; w++) {
-            if (duration <= w) {
-                dp[i][w] = Math.max(
-                    impact + dp[i - 1][w - duration],
-                    dp[i - 1][w]
-                );
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
-        }
-    }
+//         for (let w = 0; w <= maxHours; w++) {
+//             if (duration <= w) {
+//                 dp[i][w] = Math.max(
+//                     impact + dp[i - 1][w - duration],
+//                     dp[i - 1][w]
+//                 );
+//             } else {
+//                 dp[i][w] = dp[i - 1][w];
+//             }
+//         }
+//     }
 
-    return dp[n][maxHours];
-};
+//     return dp[n][maxHours];
+// };
 
-module.exports = scheduleVehicles;
+// module.exports = scheduleVehicles;
